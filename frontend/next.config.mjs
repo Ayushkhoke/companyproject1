1ba5842956026d576d26ed7daca7fc8800018def
Spartan 253 <ayushkhoke1@gmail.com>
2026-07-11 17:43:39 +0530

    Remove API key from env example

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,12 +1,6 @@
-import path from "node:path";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    useWasmBinary: true,
-  },
-  outputFileTracingRoot: path.join(process.cwd(), "../.."),
 };
 
 export default nextConfig;
